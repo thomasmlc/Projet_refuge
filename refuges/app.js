56d8eebaeeb669108a835a3586e7d778d74c99dd
Akiyoma <43410025+Akiyoma@users.noreply.github.com>
2020-05-02 17:35:52 +0200

    Initialise views

--- refuges/app.js
+++ refuges/app.js
@@ -32,12 +32,12 @@ app.post('/api/genres', function(request, response){
                 return{
                     attribute: error.path,
                     message: error.message
-                } 
+                }
             })
         })
-        
+
     })
-    
+
 })
 
 app.get('/api/refuges', function(request, response){
@@ -48,7 +48,7 @@ app.get('/api/refuges', function(request, response){
         filter = {
             where: {
                 name: {
-                    
+
                     [Op.like]: `%${q}%`
                 }
             }
@@ -71,9 +71,37 @@ app.get('/api/refuges/:id', function(request, response){
         }else{
             response.status(404).send();
         }
-        
+
 
     })
 });
 
 app.listen(8000)
+
+//**************************************************************//
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    // res.send ("Hello world...");
+    res.render("index")
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/data", (req, res) => {
+    const test = {
+        title: "Test",
+        items: ["one", "two", "three"]
+    };
+    res.render("data", { model: test });
+});
+
+app.get("/shelters", (req, res) => {
+    Refuges.findAll()
+        .then(refuges => res.render("shelters", {
+            refuges
+        }))
+        .catch(err => console.log(err))
+});
